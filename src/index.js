693ee3fb6e701b8330c3b690277754a7dc1c5f31
MahipaL-Tomar <tomarmahipalsingh6@gmail.com>
2025-06-30 23:03:54 +0530

    part 2

--- src/index.js
+++ src/index.js
@@ -1,39 +1,44 @@
-import dotenv from "dotenv"
+// require('dotenv').config()
 
-
+import dotenv from "dotenv";
 import connectDB from "./database/index.js";
 
-dotenv.config({
-    path:'./env'
-})
 
-connectDB()
+dotenv.config(); // Load from .env in root
 
+console.log("DB URI:", process.env.MONGODB_URI); // Should NOT be undefined
 
-
+connectDB();
 
 
 
 
-/*
+
+
+
+
+/*  BASICS CODE THAT HAVE FUNCTION IN INDEX.JS FILE NOT IN OTHER FILE
 import express from "express"
-const app = express()
-;(async()=>{
-    try{
-    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
 
-    app.on("error",(error)=>{
-        console.log("error",error);
-        throw error
-    })
-app.listen(process.env.PORT,()=>{
-    console.log(`app is listenning on port ${process.env.PORT}`)
-})
+const app = express()
+
+(async()=>{
+    try{
+    await mongoose.connect(`${process.env.MONGODB_URI}/$
+        {DB_NAME}
+    `)
+        app.on("express error",(error)=>{
+            console.log("Errr:",error);
+            throw error
+        })
+
+        app.listen(process.env.PORT,()=>{
+            console.log(`App is listenning on port ${process.env.PORT}`);
+        })
+
 
     }catch(error){
-        console.error("ERROR",error)
+        console.error("ERROR:",error)
         throw err
     }
-})()
-    
-*/
+})()   */
